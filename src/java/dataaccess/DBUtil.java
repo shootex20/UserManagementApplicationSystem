dba13617d5b532a304b530a1b26bb498748a7253
Chelsey Coughlin <chelsey74@live.ca>
2021-04-06 14:14:23 -0600

    DB functionality added.

--- src/java/dataaccess/DBUtil.java
+++ src/java/dataaccess/DBUtil.java
@@ -5,7 +5,7 @@ import javax.persistence.Persistence;
 
 public class DBUtil {
     private static final EntityManagerFactory emf =
-        Persistence.createEntityManagerFactory("userPU");
+        Persistence.createEntityManagerFactory("UsersPU");
 
     public static EntityManagerFactory getEmFactory() {
         return emf;
